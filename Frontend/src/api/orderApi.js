ba36eab45b30b710de1bc24a62a05ab2bb952511
Yalamanchili Rushi <rushiyalamanchili2@gmail.com>
2026-02-02 08:58:46 +0530

    Fix orders API for production using env variables

--- Frontend/src/api/orderApi.js
+++ Frontend/src/api/orderApi.js
@@ -1,25 +1,12 @@
 import axios from "axios"
 
-const API_BASE_URL = "http://localhost:3000/api/orders"
+const API_URL = import.meta.env.VITE_API_BASE_URL + "/api/orders"
 
-// Fetch orders
 export const getOrders = async (params = {}) => {
-  try {
-    const res = await axios.get(API_BASE_URL, { params })
-    return res.data
-  } catch (error) {
-    throw error
-  }
+  const res = await axios.get(API_URL, { params })
+  return res.data
 }
 
-// Update order status
 export const updateOrderStatus = async (id, status) => {
-  try {
-    const res = await axios.patch(`${API_BASE_URL}/${id}/status`, {
-      status,
-    })
-    return res.data
-  } catch (error) {
-    throw error
-  }
+  await axios.patch(`${API_URL}/${id}/status`, { status })
 }
